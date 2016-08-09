d55168d034c711ba4120d1a11e4d682b5e8b10c1
yaojianwei <yaojianwei@meicai.cn>
2016-08-09 18:22:31 +0800

    no message

---
+++ SinaWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SinaWeibo
+//
+//  Created by cain on 16/8/9.
+//  Copyright © 2016年 cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SinaWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SinaWeibo
+//
+//  Created by cain on 16/8/9.
+//  Copyright © 2016年 cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
